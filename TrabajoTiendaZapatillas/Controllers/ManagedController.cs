b6c0af80f92e367ea353fca2e2294665d5087601
javierpantoja1998 <cientificodebanco@gmail.com>
2023-03-03 14:23:32 +0100

    pagina de login visualmente

--- TrabajoTiendaZapatillas/Controllers/ManagedController.cs
+++ TrabajoTiendaZapatillas/Controllers/ManagedController.cs
@@ -30,5 +30,25 @@ namespace TrabajoTiendaZapatillas.Controllers
             return View();
         }
 
+       /* public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string usuario, string password)
+        {
+            if (usuario.ToLower() == "admin" && password.ToLower() == "admin")
+            {
+                //ALMACENAMOS EL USUARIO EN SESSION
+                HttpContext.Session.SetString("USUARIO", usuario);
+                return RedirectToAction("Productos", "Tienda");
+            }
+            else
+            {
+
+                return RedirectToAction("AccesoDenegado");
+            }
+        }*/
     }
 }
---
+++ TrabajoTiendaZapatillas/Models/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrabajoTiendaZapatillas.Models
+{
+    [Table("USUARIOS")]
+    public class Usuario
+    {
+        [Key]
+        [Column("IDZAPATILLA")]
+        public int IdUsuario { get; set; }
+
+        [Column("IDROL")]
+        public int IdRol { get; set; }
+
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+
+        [Column("DNI")]
+        public int Dni { get; set; }
+
+        [Column("DIRECCION")]
+        public string Direccion { get; set; }
+
+        [Column("TELEFONO")]
+        public int Telefono { get; set; }
+
+        [Column("EMAIL")]
+        public string Email { get; set; }
+    }
+}
