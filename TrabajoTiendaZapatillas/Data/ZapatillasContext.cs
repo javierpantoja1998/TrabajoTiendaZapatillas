43899faaeb327b0317894dafa1e1fdcfb2ef517d
javierpantoja1998 <cientificodebanco@gmail.com>
2023-03-06 09:29:20 +0100

    empezando getZapatillas

---
+++ TrabajoTiendaZapatillas/Data/ZapatillasContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrabajoTiendaZapatillas.Models;
+
+namespace TrabajoTiendaZapatillas.Data
+{
+    public class ZapatillasContext : DbContext
+    {
+        public ZapatillasContext(DbContextOptions<ZapatillasContext> options) : base(options) { }
+
+        public DbSet<Zapatilla> Zapatillas { get; set; }
+
+        
+    }
+}
---
+++ TrabajoTiendaZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,14 @@
+﻿using TrabajoTiendaZapatillas.Data;
+
+namespace TrabajoTiendaZapatillas.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+    }
+}
