e37f2909b7a2b62fd53a7c13219abf64186f5236
javierpantoja1998 <cientificodebanco@gmail.com>
2023-03-14 19:20:26 +0100

    modelo ventas creado

---
+++ TrabajoTiendaZapatillas/Models/Venta.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrabajoTiendaZapatillas.Models
+{
+    [Table("VENTAS")]
+    public class Venta
+    {
+        [Key]
+        [Column("IDVENTA")]
+        public int IdVenta { get; set; }
+
+        [Column("IDZAPATILLA")]
+        public int IdZapatilla { get; set; }
+
+        [Column("IDUSUARIO")]
+        public int IdUsuario { get; set; }
+
+        [Column("NUMEROCOMPROBANTE")]
+        public int NumeroComprobante { get; set; }
+
+        [Column("FECHA")]
+        public DateTime Fecha { get; set; }
+
+        [Column("IMPUESTO")]
+        public int Impuesto { get; set; }
+
+        [Column("TOTAL")]
+        public int Total { get; set; }
+    }
+}
