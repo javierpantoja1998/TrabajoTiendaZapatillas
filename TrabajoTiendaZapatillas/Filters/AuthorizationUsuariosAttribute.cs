228b100f534720d21bbd1b528df46d3d2a00632b
javierpantoja1998 <cientificodebanco@gmail.com>
2023-03-21 13:42:42 +0100

    empezando a crear las cosas para el login -- ME HE QUEDADO HACIENDO EL AUTHORIZATION USUARIOS

---
+++ TrabajoTiendaZapatillas/Filters/AuthorizationUsuariosAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TrabajoTiendaZapatillas.Filters
+{
+    public class AuthorizationUsuariosAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
